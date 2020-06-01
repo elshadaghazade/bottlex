4d2534c0524c7d1da039866ea9ab113449825ba2
Elshad Agayev <elshadaghazade@gmail.com>
2020-06-02 00:54:23 +0400

    commit

---
+++ precache-manifest.87b48e78606fb4b473bfedddafa0d683.js
@@ -0,0 +1,154 @@
+self.__precacheManifest = [
+  {
+    "revision": "51028b3d4728da82902d",
+    "url": "./static/js/app.6d441b7d.chunk.js"
+  },
+  {
+    "revision": "87c24ad418007cbbc123",
+    "url": "./static/js/runtime~app.e70dad09.js"
+  },
+  {
+    "revision": "9e3b6d29a080ad0031ff",
+    "url": "./static/js/2.42d48a03.chunk.js"
+  },
+  {
+    "revision": "bb3a4355b00dc4c074dcbd4664027ece",
+    "url": "./static/media/logo.bb3a4355.png"
+  },
+  {
+    "revision": "82c8fc38eb25dca10493a994ca1bfb90",
+    "url": "./static/media/no-photo.82c8fc38.png"
+  },
+  {
+    "revision": "004c2bbb035d8d06bb830efc4673c886",
+    "url": "./static/media/star.004c2bbb.png"
+  },
+  {
+    "revision": "2327736b3ea09c41abfb69be1221f430",
+    "url": "./static/media/heart.2327736b.png"
+  },
+  {
+    "revision": "51671417ef20e0bbc32f0a2bc6edfa95",
+    "url": "./static/media/rocket.51671417.png"
+  },
+  {
+    "revision": "a7b9dc9de5f8f1fb1afbef917619a5ac",
+    "url": "./static/media/bell.a7b9dc9d.png"
+  },
+  {
+    "revision": "744ce60078c17d86006dd0edabcd59a7",
+    "url": "././fonts/Entypo.ttf"
+  },
+  {
+    "revision": "ca9ce9ff0676a9b04ef0f8a3ad17dd08",
+    "url": "././fonts/Feather.ttf"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "././fonts/FontAwesome.ttf"
+  },
+  {
+    "revision": "e20945d7c929279ef7a6f1db184a4470",
+    "url": "././fonts/Foundation.ttf"
+  },
+  {
+    "revision": "b2e0fc821c6886fb3940f85a3320003e",
+    "url": "././fonts/Ionicons.ttf"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "././fonts/MaterialIcons.ttf"
+  },
+  {
+    "revision": "3c851d60ad5ef3f2fe43ebd263490d78",
+    "url": "././fonts/MaterialCommunityIcons.ttf"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "././fonts/SimpleLineIcons.ttf"
+  },
+  {
+    "revision": "3a2ba31570920eeb9b1d217cabe58315",
+    "url": "././fonts/AntDesign.ttf"
+  },
+  {
+    "revision": "5ebb24ee1112dd9562629375c387879a",
+    "url": "././fonts/Roboto-Black.ttf"
+  },
+  {
+    "revision": "99682a78fa4fe61e1177b94757336bbf",
+    "url": "././fonts/Roboto-BlackItalic.ttf"
+  },
+  {
+    "revision": "e07df86cef2e721115583d61d1fb68a6",
+    "url": "././fonts/Roboto-Bold.ttf"
+  },
+  {
+    "revision": "5b44818d2b9eda3e23cd5edd7b49b7d5",
+    "url": "././fonts/Roboto-BoldItalic.ttf"
+  },
+  {
+    "revision": "a720f17aa773e493a7ebf8b08459e66c",
+    "url": "././fonts/Roboto-Italic.ttf"
+  },
+  {
+    "revision": "88823c2015ffd5fa89d567e17297a137",
+    "url": "././fonts/Roboto-Light.ttf"
+  },
+  {
+    "revision": "a3ce4440f2abf76f4a1b14b83920138c",
+    "url": "././fonts/Roboto-LightItalic.ttf"
+  },
+  {
+    "revision": "58aef543c97bbaf6a9896e8484456d98",
+    "url": "././fonts/Roboto-Medium.ttf"
+  },
+  {
+    "revision": "cf23e1bb619029496260760b72aebd30",
+    "url": "././fonts/Roboto-MediumItalic.ttf"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "././fonts/Roboto-Regular.ttf"
+  },
+  {
+    "revision": "321de678e592d0b8f44f1a82d7ca4b62",
+    "url": "././fonts/Roboto-Thin.ttf"
+  },
+  {
+    "revision": "35a9c89aff1396595ad345e378a32aca",
+    "url": "././fonts/Roboto-ThinItalic.ttf"
+  },
+  {
+    "revision": "b70cea0339374107969eb53e5b1f603f",
+    "url": "././fonts/FontAwesome5_Solid.ttf"
+  },
+  {
+    "revision": "c39278f7abfc798a241551194f55e29f",
+    "url": "././fonts/FontAwesome5_Brands.ttf"
+  },
+  {
+    "revision": "2f9279b5c78d8e2ee3cad2ca583cfb59",
+    "url": "./static/media/ocean-bg.2f9279b5.jpg"
+  },
+  {
+    "revision": "48c7ed4e7da9792af288a60d7242d615",
+    "url": "./expo-service-worker.js"
+  },
+  {
+    "revision": "ec543248d7b23864564429fc03837190",
+    "url": "./serve.json"
+  },
+  {
+    "revision": "1e322f2477602946edc972acfa9d12f4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c075eb71bd54e614de4fa872213fec06",
+    "url": "./manifest.json"
+  },
+  {
+    "revision": "d540f5fcbad5c5b1e34efee1e7025eb4",
+    "url": "./register-service-worker.js"
+  }
+];
